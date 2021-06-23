6b0acf2991a563c2ebf23e0a2af633a8e1ecf748
masoud-moghini <koosha_pooyeshgar@outlook.com>
2021-06-23 15:52:16 +0430

    try to dockerize

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistance/ProductRepository.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistance/ProductRepository.java
@@ -1,8 +1,9 @@
-package se.magnus.microservices.core.product.repository;
+package se.magnus.microservices.core.product.persistance;
 
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 import se.magnus.microservices.core.product.persistance.ProductEntity;
 
--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -1,11 +1,11 @@
-package se.magnus.microservices.core.recommendation.repository;
+package se.magnus.microservices.core.recommendation.persistence;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import se.magnus.microservices.core.recommendation.persistence.RecommendationEntity;
 
 import java.util.List;
+
 
 public interface RecommendationRepository extends ReactiveCrudRepository<RecommendationEntity,String> {
 
